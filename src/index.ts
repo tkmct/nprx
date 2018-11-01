d4766056501aaf4cdfd5a5052ec8f217d1522708
Takamichi-tsutsumi <1220t.takamichi@gmail.com>
2018-11-01 23:36:15 +0900

    Initial TCP server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-function main() {
-  return 1
-}
+import * as net from 'net'
 
-main()
+const server = net.createServer(client => {
+  client.on('data', data => {
+    console.log('Server received data: ', data)
+  })
+})
+
+server.listen(20000, () => {
+  server.on('close', () => {
+    console.log('TCP server socket is closed.')
+  })
+})
