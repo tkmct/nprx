c962938751e0c4385c566423568b34f1f341c139
Takamichi-tsutsumi <1220t.takamichi@gmail.com>
2018-11-02 16:57:55 +0900

    Read conf file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
+import * as fs from 'fs'
 import * as net from 'net'
+
+const DEFAULT_CONF_PATH = '/Users/uu107017/.ghq/github.com/tkmct/nprx'
+const CONF_PATH = process.env.NPRX_CONF_PATH || DEFAULT_CONF_PATH
+
+const confData = fs.readFileSync(CONF_PATH)
+console.info(confData.toString('utf-8', 0, confData.length))
 
 const server = net.createServer(client => {
   client.on('data', data => {
