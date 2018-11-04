e7c6617913c4901fa170869ed1f6de2c6752e8a2
Takamichi-tsutsumi <1220t.takamichi@gmail.com>
2018-11-04 17:24:33 +0900

    pass received data from source to target

--- src/proxyWorker.ts
+++ src/proxyWorker.ts
@@ -4,26 +4,51 @@ import { Process, ProxyConfig } from './types'
 export class ProxyWorker {
   private source: Process
   private target: Process
+  private server: net.Server
 
   constructor({ source, target }: ProxyConfig) {
     this.source = source
     this.target = target
-
-    console.log(this.target)
-  }
-
-  // Start proxy server
-  public start() {
-    const server = net.createServer(client => {
-      client.on('data', data => {
-        console.log('Server received data: ', data)
+    this.server = net.createServer(sock => {
+      sock.on('data', (data: Buffer) => {
+        this.onReceiveData(data, sock)
       })
     })
+  }
 
-    server.listen(this.source.port, () => {
-      server.on('close', () => {
-        console.log('TCP server socket is closed.')
-      })
+  public onReceiveData = (sdata: Buffer, sock: net.Socket) => {
+    console.log('Server received data: ')
+    console.log(sdata.toString())
+    const connection = net.createConnection(
+      {
+        host: this.target.host || 'localhost', // TODO: set default host for target object
+        port: this.target.port
+      },
+      () => {
+        connection.write(sdata)
+      }
+    )
+
+    connection.on('data', cdata => {
+      sock.write(cdata.toString())
+      connection.end()
+    })
+
+    connection.on('end', () => {
+      console.log('disconnected from server')
+    })
+  }
+
+  public start = () => {
+    this.server.listen({
+      host: this.source.host || 'localhost', // TODO: set default host for source object
+      port: this.source.port
+    })
+  }
+
+  public close() {
+    this.server.close(() => {
+      console.log('Server is closed')
     })
   }
 }
